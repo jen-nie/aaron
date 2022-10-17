054ec4cc91dcc6d605a35fc9519700c6f6467e5f
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2022-10-17 21:42:50 +0200

    adds mobile menu + hamburger animation

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,13 @@ const nav = document.querySelector('#nav');
 button.addEventListener('click', function() {
     if (nav.classList.contains('mobile-hidden')) {
         nav.classList.remove('mobile-hidden');
-        nav.classList.add('flex');
+        nav.classList.add('shown');
+        button.querySelectorAll('div.line')[0].classList.add('line1');
+        button.querySelectorAll('div.line')[1].classList.add('line2');
     } else {
-        nav.classList.remove('flex');
+        nav.classList.remove('shown');
         nav.classList.add('mobile-hidden');
+        button.querySelectorAll('div.line')[0].classList.remove('line1');
+        button.querySelectorAll('div.line')[1].classList.remove('line2');
     }
 })
